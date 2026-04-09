805f2d7f6d0cc4ab8b6ecf837c91fff824b272df
swa1245 <swarajpatil2123@gmail.com>
2026-04-09 11:21:06 +0530

    setup done

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import app from './index.js'
+
+const PORT = process.env.PORT ;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
